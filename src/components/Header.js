61afc91717c1869d48c47e6fb7199f57b5b1917a
franklin <franklinbenjamin032@gmail.com>
2022-04-04 16:11:01 -0400

    linter fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import * as url from '../assets/planet.png';
 
-const Header = () => {
-  return (
-    <>
-    </>
-  );
-};
+const Header = () => (
+  <>
+    <img src={url} alt="space-logo" />
+  </>
+);
 
 export default Header;
