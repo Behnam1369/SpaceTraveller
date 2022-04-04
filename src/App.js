22e91d48578cfd953c9d4a33669102674c2f5f06
behnam1369 <behnam.aghaali@yahoo.com>
2022-04-05 01:26:01 +0430

    Add routing and navigation to the my profile component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import MyProfile from './components/MyProfile';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/MyProfile" element={<MyProfile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/MyProfile.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+function MyProfile() {
+  return (
+    <>
+      <h1>Hello from My Profile</h1>
+    </>
+  );
+}
+
+export default MyProfile;
