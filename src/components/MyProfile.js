1c55b08d46d0ee5f7218adf8a52be6dffb5a3d86
behnam1369 <behnam.aghaali@yahoo.com>
2022-04-07 14:51:00 +0430

    Add joined missions to the my profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,33 @@
 import { React } from 'react';
+import { useSelector } from 'react-redux';
 
 function MyProfile() {
+  const { missions } = useSelector((state) => state.missions);
+  console.log(missions);
+  const joinedMissions = missions.filter((el) => el.joined);
+  console.log(joinedMissions);
   return (
     <>
       <h1>Hello from My Profile</h1>
+
+      <div className="JoinedMissions">
+        {joinedMissions.length > 0
+          ? (
+            <>
+              <h2>My Missions</h2>
+              <ul>
+                {joinedMissions.map((el) => (
+                  <li key={el.mission_id}>
+                    {' '}
+                    {el.mission_name}
+                    {' '}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )
+          : <h2>You have not joined any missions yet.</h2>}
+      </div>
     </>
   );
 }
